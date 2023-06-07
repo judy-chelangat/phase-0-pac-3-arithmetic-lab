1c24a0576b4c02bd394dec6d0f2c319c7b212808
judy-chelangat <judysigilai75@gmail.com>
2023-06-07 19:38:47 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(x,y){
+    return (x+y)
+}
+function subtract(x,y){
+    return (x-y)
+}
+function multiply(x,y){
+    return (x*y)
+}
+function divide(x,y){
+    return (x/y)
+}
+function makeInt(string){
+   return (parseInt(string, 10));
+    
+}
+
+makeInt("123");
+
+function preserveDecimal(string){
+   return parseFloat(string);
+}
+function increment(n){
+    return(++n)
+}
+function decrement(n){
+    return(--n)
+}
